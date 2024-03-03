39ad5262c13ee5ff3c1949fbdb409d0608f5fe52
Martin Vejmelka <vejmelkam@gmail.com>
2024-03-03 10:46:51 -0700

    chore: factored out spectrogram component

--- src/App.tsx
+++ src/App.tsx
@@ -1,142 +1,38 @@
 import "./App.css";
-import { useRef, useState } from "react";
+import { useState } from "react";
+import Spectrogram from "./components/Spectrogram";
 
 export default function App() {
   const fftSize = 1024;
-  let animationController = 0;
 
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const source = useRef<AudioNode>();
-  const analyzer = useRef<AnalyserNode>();
-  const stream = useRef<MediaStream>();
-  const canvasPos = useRef(0);
-  const [recording, setRecording] = useState(false);
   const [showFreqs, setShowFreqs] = useState(512);
+  const [stream, setStream] = useState<MediaStream | undefined>(undefined);
 
-  const generateHeatMapColors = (): Uint8ClampedArray[] => {
-    const colors = new Array(256);
-    for (let i = 0; i < 256; i++) {
-      const ratio = i / 255;
-      const red = Math.floor(255 * ratio);
-      const green = Math.floor(255 * (1 - ratio));
-      const blue = 128;
-      colors[i] = new Uint8ClampedArray([red, green, blue, 255]); // RGBA
-    }
-    return colors;
-  };
-
-  const heatMap = generateHeatMapColors();
-
-  const handleAudioRecord = async () => {
-    const audioContext = new AudioContext();
-    if (recording) {
-      source.current = undefined;
-      analyzer.current = undefined;
-      stream.current?.getAudioTracks().forEach((track) => track.stop());
-      stream.current = undefined;
-      setRecording(false);
-      cancelAnimationFrame(animationController);
+  const updateRecordingStatus = (newStatus: boolean) => {
+    if (newStatus) {
+      navigator.mediaDevices
+        .getUserMedia({
+          audio: true,
+          video: false,
+        })
+        .then((newStream: MediaStream) => {
+          setStream(newStream);
+        });
     } else {
-      setRecording(true);
-      stream.current = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: false,
-      });
-      if (!source.current) {
-        source.current = audioContext.createMediaStreamSource(stream.current);
-        analyzer.current = audioContext.createAnalyser();
-        analyzer.current.fftSize = fftSize;
-        source.current.connect(analyzer.current);
-      }
-      // visualizeData();
-      incrementSpectrogram();
+      stream!.getAudioTracks().forEach((track) => track.stop());
+      setStream(undefined);
     }
   };
 
-  const renderSpectrogramLine = (
-    freqIntensities: Uint8Array,
-    ctx: CanvasRenderingContext2D,
-    x: number,
-    height: number
-  ) => {
-    const freqCount = freqIntensities.length;
-    const imageData = ctx.createImageData(1, height);
-
-    const step = height / freqCount;
-
-    freqIntensities.forEach((value, index) => {
-      const color = heatMap[value];
-      const pos = (height - step - index * step) * 4;
-      for (let i = 0; i < step; i++) {
-        imageData.data.set(color, pos + i * 4);
-      }
-    });
-
-    ctx.putImageData(imageData, x, 0);
-  };
-
-  const incrementSpectrogram = () => {
-    const stft = new Uint8Array(fftSize / 2);
-    analyzer.current!.getByteFrequencyData(stft);
-
-    const canvas = canvasRef.current!;
-    const ctx = canvas.getContext("2d")!;
-
-    renderSpectrogramLine(
-      stft.slice(0, showFreqs),
-      ctx,
-      canvasPos.current,
-      canvas.height
-    );
-    canvasPos.current = (canvasPos.current + 1) % canvas.width;
-
-    // this makes the rest of the canvas darker for some reason
-    /*
-    if(canvasPos.current < canvas.width) {
-      ctx.lineWidth = 1;
-      ctx.strokeStyle = "black";
-      ctx.globalAlpha = 255;
-      ctx.beginPath();
-      const x = canvasPos.current;
-      ctx.moveTo(x, 0);
-      ctx.lineTo(x, canvas.height);
-      ctx.stroke();
-    }
-    */
-
-    animationController = requestAnimationFrame(incrementSpectrogram);
-  };
-
-  /*
-  const visualizeData = () => {
-    const stft = new Uint8Array(fftSize / 2);
-    analyzer.current !.getByteFrequencyData(stft);
-    const bar_step = Math.floor(1024 / stft.length);
-    const bar_width = Math.floor((1024 / stft.length) * 0.75);
-    const canvas = canvasRef.current !;
-    const ctx = canvas.getContext("2d") !;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < stft.length; i++) {
-      // compute x coordinate where we would draw
-      const start = i * bar_step;
-      // create a gradient for the  whole canvas
-      const gradient =
-          ctx.createLinearGradient(0, 0, canvas.width, canvas.height, );
-      gradient.addColorStop(0.2, "#2392f5");
-      gradient.addColorStop(0.5, "#fe0095");
-      gradient.addColorStop(1.0, "purple");
-      ctx.fillStyle = gradient;
-      ctx.fillRect(start, canvas.height, bar_width, -stft[i]);
-    }
-
-    animationController = window.requestAnimationFrame(visualizeData);
-  };
-  */
+  const recording = stream != undefined;
 
   return (
     <div className="App">
       <div style={{ margin: "1em" }}>
-        <button style={{ margin: "0.5em" }} onClick={handleAudioRecord}>
+        <button
+          style={{ margin: "0.5em" }}
+          onClick={() => updateRecordingStatus(!recording)}
+        >
           {recording ? "Stop" : "Start"}
         </button>
         <select
@@ -151,11 +47,11 @@ export default function App() {
         </select>
       </div>
       <div>
-        <canvas
-          ref={canvasRef}
-          width={786}
-          height={512}
-          style={{ border: "1px solid #ffffff" }}
+        <Spectrogram
+          showFreqs={showFreqs}
+          fftSize={fftSize}
+          stream={stream}
+          recording={recording}
         />
       </div>
     </div>
---
+++ src/components/Spectrogram.tsx
@@ -0,0 +1,114 @@
+import { useRef } from "react";
+
+interface SpectrogramProps {
+  fftSize: number;
+  showFreqs: number;
+  recording: boolean;
+  stream: MediaStream | undefined;
+}
+
+export default function Spectrogram(props: SpectrogramProps) {
+  let animationController = -1;
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const source = useRef<AudioNode>();
+  const analyzer = useRef<AnalyserNode>();
+  const canvasPos = useRef(0);
+
+  console.log(props);
+
+  const generateHeatMapColors = (): Uint8ClampedArray[] => {
+    const colors = new Array(256);
+    for (let i = 0; i < 256; i++) {
+      const ratio = i / 255;
+      const red = Math.floor(255 * ratio);
+      const green = Math.floor(255 * (1 - ratio));
+      const blue = 128;
+      colors[i] = new Uint8ClampedArray([red, green, blue, 255]); // RGBA
+    }
+    return colors;
+  };
+
+  const heatMap = generateHeatMapColors();
+
+  // block executes on construction
+  const renderSpectrogramLine = (
+    freqIntensities: Uint8Array,
+    ctx: CanvasRenderingContext2D,
+    x: number,
+    height: number
+  ) => {
+    const freqCount = freqIntensities.length;
+    const imageData = ctx.createImageData(1, height);
+
+    const step = height / freqCount;
+
+    freqIntensities.forEach((value, index) => {
+      const color = heatMap[value];
+      const pos = (height - step - index * step) * 4;
+      for (let i = 0; i < step; i++) {
+        imageData.data.set(color, pos + i * 4);
+      }
+    });
+
+    ctx.putImageData(imageData, x, 0);
+  };
+
+  const incrementSpectrogram = () => {
+    if (!canvasRef.current || !analyzer.current) return;
+
+    const stft = new Uint8Array(props.fftSize / 2);
+    analyzer.current!.getByteFrequencyData(stft);
+
+    const canvas = canvasRef.current!;
+    const ctx = canvas.getContext("2d")!;
+
+    renderSpectrogramLine(
+      stft.slice(0, props.showFreqs),
+      ctx,
+      canvasPos.current,
+      canvas.height
+    );
+    canvasPos.current = (canvasPos.current + 1) % canvas.width;
+
+    // this makes the rest of the canvas darker for some reason
+    /*
+    if(canvasPos.current < canvas.width) {
+      ctx.lineWidth = 1;
+      ctx.strokeStyle = "black";
+      ctx.globalAlpha = 255;
+      ctx.beginPath();
+      const x = canvasPos.current;
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, canvas.height);
+      ctx.stroke();
+    }
+    */
+
+    animationController = requestAnimationFrame(incrementSpectrogram);
+  };
+
+  const audioContext = new AudioContext();
+  if (props.recording) {
+    if (props.stream && !source.current) {
+      source.current = audioContext.createMediaStreamSource(props.stream!);
+      analyzer.current = audioContext.createAnalyser();
+      analyzer.current.fftSize = props.fftSize;
+      source.current.connect(analyzer.current);
+      if (animationController < 0) incrementSpectrogram();
+    }
+  } else {
+    source.current = undefined;
+    analyzer.current = undefined;
+    cancelAnimationFrame(animationController);
+    animationController = -1;
+  }
+
+  return (
+    <canvas
+      ref={canvasRef}
+      width={786}
+      height={512}
+      style={{ border: "1px solid #ffffff" }}
+    />
+  );
+}
